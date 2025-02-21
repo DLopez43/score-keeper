74e5c3ed8c9122bdd793f68f7c56cbbdb6d30c45
Delilah Lopez <Delilah.marinaL@gmail.com>
2025-02-21 16:25:14 -0500

    Created logic to update player1 count and h1 textContent

---
+++ count.js
@@ -0,0 +1,13 @@
+let player1Score = 0
+let player2Score = 0
+
+let firstPlayer= document.querySelector('#player1')
+let secondPlayer= document.querySelector('#player2')
+let zeroed= document.querySelector('#reset')
+let h1 = document.querySelector('h1')
+
+firstPlayer.addEventListener('click', function (e) {
+    console.log('CLICK')
+    player1Score += 1
+    h1.textContent = `${player1Score} to ${player2Score}`
+})
